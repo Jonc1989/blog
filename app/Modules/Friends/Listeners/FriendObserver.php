cf7f093ca320a34b1ac4c74df08aaf771ada254b
Janis Mozais <janis.mozais@inbox.lv>
2016-08-25 22:17:40 +0300

    friends revision

--- app/Modules/Friends/Listeners/FriendObserver.php
+++ app/Modules/Friends/Listeners/FriendObserver.php
@@ -2,27 +2,47 @@
 
 use App\Modules\Friends\Models\Friends;
 
+use App\Http\Models\Revisions;
 class FriendObserver
 {
+    public function __construct( Revisions $revisions )
+    {
+        $this->revision = $revisions;
+    }
+
     /**
      * Listen to the User created event.
      *
      * @param  User  $user
      * @return void
      */
-    public function created(Friends $user)
+    public function created(Friends $model)
     {
-        \Log::info( $user );
+
+        $class = new \ReflectionClass($model);
+
+        \Log::info($class->getName());
+
+
+
+        //\Log::info( 'created' );
+        ///\Log::info( $user );
+        $this->revision->create([
+            'revisionable_type'     => $class->getName(),
+            'revisionable_id'       => $model->id,
+            'user_id'               => \Auth::id()
+        ]);
     }
 
     /**
-     * Listen to the User deleting event.
+     * Listen to the User updating event.
      *
      * @param  User  $user
      * @return void
      */
-    public function deleting(Friends $user)
+    public function updated(Friends $user)
     {
+        \Log::info( 'updated' );
         \Log::info( $user );
     }
 
@@ -32,8 +52,23 @@ class FriendObserver
      * @param  User  $user
      * @return void
      */
-    public function updating(Friends $user)
+    public function saved(Friends $user)
     {
+        //\Log::info( 'saved' );
+        //\Log::info( $user );
+    }
+
+    /**
+     * Listen to the User deleting event.
+     *
+     * @param  User  $user
+     * @return void
+     */
+    public function deleted(Friends $user)
+    {
+        \Log::info( 'deleting' );
         \Log::info( $user );
     }
+
+
 }
