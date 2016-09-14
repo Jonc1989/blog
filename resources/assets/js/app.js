db25a6aff5d6ec99556dcc350dd6eaa4f8d04dbc
Janis Mozais <janis.mozais@inbox.lv>
2016-09-14 17:07:07 +0300

    ...

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -10,4 +10,34 @@ var app = angular.module( 'app', [
     'galleries',
     'comments',
     'ngToast'
-] );
+] )
+    .config([ '$httpProvider', function( $httpProvider )
+        {
+            var interceptor = ['$rootScope', '$q', '$window', function(scope, $q, $window) {
+                function success(response) {
+                    return response;
+                }
+
+                function error(response) {
+                    var status = response.status;
+                    if (status == 401) {
+                        window.location.replace( "/" );
+                        console.log('logout');
+                        return;
+                    }
+                    //if (status == 500) {
+                    //    window.location.replace( "/" );
+                    //    return;
+                    //}
+                    // otherwise
+                    return $q.reject(response);
+                }
+
+                return function(promise) {
+                    return promise.then(success, error);
+                }
+            }];
+
+            $httpProvider.interceptors.push(interceptor);
+
+        }]);
